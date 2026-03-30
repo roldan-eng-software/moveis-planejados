65e57358529fa12447962ee2a824ad53637d126e
Sandro Roldan <roldan.eng.software@gmail.com>
2026-03-30 19:00:50 -0300

    Commit - Login

--- src/lib/stack.ts
+++ src/lib/stack.ts
@@ -4,6 +4,7 @@ export const stackClientApp = new StackClientApp({
   projectId: process.env.NEXT_PUBLIC_STACK_PROJECT_ID!,
   publishableClientKey: process.env.NEXT_PUBLIC_STACK_PUBLISHABLE_CLIENT_KEY!,
   tokenStore: "cookie",
+  redirectAfterSignIn: "/app/dashboard",
 });
 
 export const stackServerApp = stackClientApp;
